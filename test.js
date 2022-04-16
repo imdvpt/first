94410f52cd9f4c20aaaa6851fc83e930231bdc19
imdvpt <103763729+imdvpt@users.noreply.github.com>
2022-04-16 17:40:19 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ const server=http.createServer((require,response)=>{
     response.end("helloworld!!!!!!!");
 });
 const port=3000;
-const ip="127.0.0.1";
-server.listen(port,ip,()=>{
+server.listen(port,()=>{
     console.log("server is run..");
 })
